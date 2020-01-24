1a78d6a8ded12ec976bcb59a1bd0aef5773232f2
Weverton Marques <wevertonms@gmail.com>
2020-01-24 16:58:25 -0300

    Formatted download.js

--- download.js
+++ download.js
@@ -1,15 +1,15 @@
-const file_name = source.data['file_name'][0]
-file_text = source.data['html'][0].toString()
-const blob = new Blob([file_text], { type: 'text/csv;charset=utf-8;' })
+const file_name = source.data["file_name"][0];
+file_text = source.data["html"][0].toString();
+const blob = new Blob([file_text], { type: "text/csv;charset=utf-8;" });
 
 //addresses IE
 if (navigator.msSaveBlob) {
-    navigator.msSaveBlob(blob, file_name)
+  navigator.msSaveBlob(blob, file_name);
 } else {
-    const link = document.createElement('a')
-    link.href = URL.createObjectURL(blob)
-    link.download = file_name
-    link.target = '_blank'
-    link.style.visibility = 'hidden'
-    link.dispatchEvent(new MouseEvent('click'))
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(blob);
+  link.download = file_name;
+  link.target = "_blank";
+  link.style.visibility = "hidden";
+  link.dispatchEvent(new MouseEvent("click"));
 }
