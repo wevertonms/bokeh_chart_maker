97feee34514420aadf541a60c39b155a6ba78574
Weverton Marques <wevertonms@gmail.com>
2020-01-24 17:07:10 -0300

    Update download.js

--- download.js
+++ download.js
@@ -6,11 +6,11 @@ const blob = new Blob([fileText], { type: "text/csv;charset=utf-8;" });
 
 //addresses IE
 if (navigator.msSaveBlob) {
-  navigator.msSaveBlob(blob, file_name);
+  navigator.msSaveBlob(blob, fileName);
 } else {
   const link = document.createElement("a");
   link.href = URL.createObjectURL(blob);
-  link.download = file_name;
+  link.download = fileName;
   link.target = "_blank";
   link.style.visibility = "hidden";
   link.dispatchEvent(new MouseEvent("click"));
